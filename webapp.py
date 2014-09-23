5bc4d8b4d0fd8d2edd4bc8edf7c84128bb79bda7
kr4ng <steven.m.simoni@gmail.com>
2014-09-23 08:39:14 -0700

    d

--- webapp.py
+++ webapp.py
@@ -5,16 +5,18 @@ import os
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	return "Hello World!"
 
 # add a new route
 @app.route('/canvas', methods=['POST'])
 def canvas():
-  secret = os.environ.get('SECRET')
-  sr_param = request.form['signed_request']
-  srHelper = SignedRequest(secret,sr_param)
-  canvasRequestJSON = srHelper.verifyAndDecode()
-  return canvasRequestJSON
+	if request.form['contactzip']:
+		return request.form['contactzip']
+	secret = os.environ.get('SECRET')
+	sr_param = request.form['signed_request']
+	srHelper = SignedRequest(secret,sr_param)
+	canvasRequestJSON = srHelper.verifyAndDecode()
+	return canvasRequestJSON
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
