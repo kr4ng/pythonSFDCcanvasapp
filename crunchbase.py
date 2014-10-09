1ea8f8983c71ee3c9f0300094f03f986bb6a4c8f
kr4ng <steven.m.simoni@gmail.com>
2014-10-09 13:53:59 -0700

    d

---
+++ crunchbase.py
@@ -0,0 +1,15 @@
+'''
+kr4ng at ssimoni in ~/Projects/restmarketo
+$ curl -v  -X GET "http://api.crunchbase.com/v/2/organization/marketo?user_key=2f67915d9dc64a88d4f2ff0ba1dfd3e8"
+* Adding handle: conn: 0x7fdad900a600
+* Adding handle: send: 0
+* Adding handle: recv: 0
+* Curl_addHandleToPipeline: length: 1
+* - Conn 0 (0x7fdad900a600) send_pipe: 1, recv_pipe: 0
+* About to connect() to api.crunchbase.com port 80 (#0)
+*   Trying 54.241.187.53...
+* Connected to api.crunchbase.com (54.241.187.53) port 80 (#0)
+> GET /v/2/organization/marketo?user_key=2f67915d9dc64a88d4f2ff0ba1dfd3e8 HTTP/1.1
+> User-Agent: curl/7.30.0
+'''
+
--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from canvas_signed_request import SignedRequest
 app = Flask(__name__)
 import os
@@ -16,111 +16,112 @@ def canvas():
 	srHelper = SignedRequest(secret,sr_param)
 	canvasRequestJSON = srHelper.verifyAndDecode()
 	canvasRequest = json.loads(canvasRequestJSON)
+	contactID = canvasRequest['context']['environment']['record']['Id']
 	print canvasRequest['context']['environment']['record']['Id']
-	return canvasRequest['context']['environment']['record']['Id']
+	return render_template('hellodfpython.html', contactID = contactID)
 
 if __name__ == "__main__":
 	app.run()
 	'''
 	{  
-   "algorithm":"HMACSHA256",
-   "issuedAt":-1555461611,
-   "userId":"005i0000001CPTiAAO",
-   "client":{  
-      "refreshToken":null,
-      "instanceId":"_:python_salesforce_test:j_id0:j_id1:canvasapp",
-      "targetOrigin":"https://c.na15.visual.force.com",
-      "instanceUrl":"https://na15.salesforce.com",
-      "oauthToken":"00Di0000000dulO!AQsAQBW2erVp284P5sxb3hJ1h_4EWCJbZVlVrazphaX3AN06nps8mdPooT_P2ywIjiEv06nxFbKUOoZkP7qxBIvlLA4w_qr0"
-   },
-   "context":{  
-      "user":{  
-         "userId":"005i0000001CPTiAAO",
-         "userName":"steven.m.simoni@gmail.com",
-         "firstName":"Steven",
-         "lastName":"Simoni",
-         "email":"steven.m.simoni@gmail.com",
-         "fullName":"Steven Simoni",
-         "locale":"en_US",
-         "language":"en_US",
-         "timeZone":"America/Los_Angeles",
-         "profileId":"00ei00000013FPB",
-         "roleId":null,
-         "userType":"STANDARD",
-         "currencyISOCode":"USD",
-         "profilePhotoUrl":"https://c.na15.content.force.com/profilephoto/005/F",
-         "profileThumbnailUrl":"https://c.na15.content.force.com/profilephoto/005/T",
-         "siteUrl":null,
-         "siteUrlPrefix":null,
-         "networkId":null,
-         "accessibilityModeEnabled":false,
-         "isDefaultNetwork":true
-      },
-      "links":{  
-         "loginUrl":"https://login.salesforce.com/",
-         "enterpriseUrl":"/services/Soap/c/31.0/00Di0000000dulO",
-         "metadataUrl":"/services/Soap/m/31.0/00Di0000000dulO",
-         "partnerUrl":"/services/Soap/u/31.0/00Di0000000dulO",
-         "restUrl":"/services/data/v31.0/",
-         "sobjectUrl":"/services/data/v31.0/sobjects/",
-         "searchUrl":"/services/data/v31.0/search/",
-         "queryUrl":"/services/data/v31.0/query/",
-         "recentItemsUrl":"/services/data/v31.0/recent/",
-         "chatterFeedsUrl":"/services/data/v31.0/chatter/feeds",
-         "chatterGroupsUrl":"/services/data/v31.0/chatter/groups",
-         "chatterUsersUrl":"/services/data/v31.0/chatter/users",
-         "chatterFeedItemsUrl":"/services/data/v31.0/chatter/feed-items",
-         "userUrl":"/005i0000001CPTiAAO"
-      },
-      "application":{  
-         "namespace":null,
-         "name":"python salesforce test",
-         "canvasUrl":"https://sfcanvaspythonapptest.herokuapp.com/canvas",
-         "applicationId":"06Pi0000000Ik9T",
-         "version":"1.0",
-         "authType":"SIGNED_REQUEST",
-         "referenceId":"09Hi00000008jRM",
-         "options":[  
+	"algorithm":"HMACSHA256",
+	"issuedAt":-1555461611,
+	"userId":"005i0000001CPTiAAO",
+	"client":{  
+		"refreshToken":null,
+		"instanceId":"_:python_salesforce_test:j_id0:j_id1:canvasapp",
+		"targetOrigin":"https://c.na15.visual.force.com",
+		"instanceUrl":"https://na15.salesforce.com",
+		"oauthToken":"00Di0000000dulO!AQsAQBW2erVp284P5sxb3hJ1h_4EWCJbZVlVrazphaX3AN06nps8mdPooT_P2ywIjiEv06nxFbKUOoZkP7qxBIvlLA4w_qr0"
+	},
+	"context":{  
+		"user":{  
+			"userId":"005i0000001CPTiAAO",
+			"userName":"steven.m.simoni@gmail.com",
+			"firstName":"Steven",
+			"lastName":"Simoni",
+			"email":"steven.m.simoni@gmail.com",
+			"fullName":"Steven Simoni",
+			"locale":"en_US",
+			"language":"en_US",
+			"timeZone":"America/Los_Angeles",
+			"profileId":"00ei00000013FPB",
+			"roleId":null,
+			"userType":"STANDARD",
+			"currencyISOCode":"USD",
+			"profilePhotoUrl":"https://c.na15.content.force.com/profilephoto/005/F",
+			"profileThumbnailUrl":"https://c.na15.content.force.com/profilephoto/005/T",
+			"siteUrl":null,
+			"siteUrlPrefix":null,
+			"networkId":null,
+			"accessibilityModeEnabled":false,
+			"isDefaultNetwork":true
+		},
+		"links":{  
+			"loginUrl":"https://login.salesforce.com/",
+			"enterpriseUrl":"/services/Soap/c/31.0/00Di0000000dulO",
+			"metadataUrl":"/services/Soap/m/31.0/00Di0000000dulO",
+			"partnerUrl":"/services/Soap/u/31.0/00Di0000000dulO",
+			"restUrl":"/services/data/v31.0/",
+			"sobjectUrl":"/services/data/v31.0/sobjects/",
+			"searchUrl":"/services/data/v31.0/search/",
+			"queryUrl":"/services/data/v31.0/query/",
+			"recentItemsUrl":"/services/data/v31.0/recent/",
+			"chatterFeedsUrl":"/services/data/v31.0/chatter/feeds",
+			"chatterGroupsUrl":"/services/data/v31.0/chatter/groups",
+			"chatterUsersUrl":"/services/data/v31.0/chatter/users",
+			"chatterFeedItemsUrl":"/services/data/v31.0/chatter/feed-items",
+			"userUrl":"/005i0000001CPTiAAO"
+		},
+		"application":{  
+			"namespace":null,
+			"name":"python salesforce test",
+			"canvasUrl":"https://sfcanvaspythonapptest.herokuapp.com/canvas",
+			"applicationId":"06Pi0000000Ik9T",
+			"version":"1.0",
+			"authType":"SIGNED_REQUEST",
+			"referenceId":"09Hi00000008jRM",
+			"options":[  
 
-         ],
-         "samlInitiationMethod":"None",
-         "developerName":"python_salesforce_test"
-      },
-      "organization":{  
-         "organizationId":"00Di0000000dulOEAQ",
-         "name":"Marketo",
-         "multicurrencyEnabled":false,
-         "namespacePrefix":null,
-         "currencyIsoCode":"USD"
-      },
-      "environment":{  
-         "locationUrl":"https://c.na15.visual.force.com/servlet/servlet.Integration?lid=066i0000004JMXr&ic=1",
-         "displayLocation":"Visualforce",
-         "sublocation":null,
-         "uiTheme":"Theme3",
-         "dimensions":{  
-            "width":"500px",
-            "height":"100px",
-            "maxWidth":"1000px",
-            "maxHeight":"2000px",
-            "clientWidth":"1148px",
-            "clientHeight":"30px"
-         },
-         "parameters":{  
+			],
+			"samlInitiationMethod":"None",
+			"developerName":"python_salesforce_test"
+		},
+		"organization":{  
+			"organizationId":"00Di0000000dulOEAQ",
+			"name":"Marketo",
+			"multicurrencyEnabled":false,
+			"namespacePrefix":null,
+			"currencyIsoCode":"USD"
+		},
+		"environment":{  
+			"locationUrl":"https://c.na15.visual.force.com/servlet/servlet.Integration?lid=066i0000004JMXr&ic=1",
+			"displayLocation":"Visualforce",
+			"sublocation":null,
+			"uiTheme":"Theme3",
+			"dimensions":{  
+				"width":"500px",
+				"height":"100px",
+				"maxWidth":"1000px",
+				"maxHeight":"2000px",
+				"clientWidth":"1148px",
+				"clientHeight":"30px"
+			},
+			"parameters":{  
 
-         },
-         "record":{  
-            "Id":"003i000000IMVhXAAX",
-            "attributes":{  
-               "type":"Contact",
-               "url":"/services/data/v31.0/sobjects/Contact/003i000000IMVhXAAX"
-            }
-         },
-         "version":{  
-            "season":"SUMMER",
-            "api":"31.0"
-         }
-      }
-   }
+			},
+			"record":{  
+				"Id":"003i000000IMVhXAAX",
+				"attributes":{  
+					"type":"Contact",
+					"url":"/services/data/v31.0/sobjects/Contact/003i000000IMVhXAAX"
+				}
+			},
+			"version":{  
+				"season":"SUMMER",
+				"api":"31.0"
+			}
+		}
+	}
 }
 	'''
